dbbed3e40ce4e15238c60dc42d485cb7ba9d3dc4
herald27701 <quochuy181013@gmail.com>
2023-01-16 14:06:39 +0700

    icon added 1

--- app/src/main/java/com/example/androidweek_2/MainActivity.kt
+++ app/src/main/java/com/example/androidweek_2/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.androidweek_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton: Button = findViewById(R.id.login_button)
+        loginButton.setOnClickListener{ login() }
+    }
+
+    private fun login()
+    {
+        var username: EditText = findViewById(R.id.email_input)
+        var password: EditText = findViewById(R.id.password_input)
+        if ("@gmail.com" in username.getText().toString())
+        {
+            Toast.makeText(this, "correct  format", Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            Toast.makeText(this, "wrong email format", Toast.LENGTH_SHORT).show()
+        }
     }
 }
